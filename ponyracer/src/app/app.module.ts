15385fa030397029e6708bf878666f4004045de9
boboch86 <40466697+boboch86@users.noreply.github.com>
2020-09-07 17:05:46 +0200

    Exercise 8 - HTTP

--- ponyracer/src/app/app.module.ts
+++ ponyracer/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RacesComponent } from './races/races.component';
 import { RaceComponent } from './race/race.component';
 import { PonyComponent } from './pony/pony.component';
+import { FromNowPipe } from './from-now.pipe';
 
 @NgModule({
   declarations: [
@@ -13,10 +15,12 @@ import { PonyComponent } from './pony/pony.component';
     MenuComponent,
     RacesComponent,
     RaceComponent,
-    PonyComponent
+    PonyComponent,
+    FromNowPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
