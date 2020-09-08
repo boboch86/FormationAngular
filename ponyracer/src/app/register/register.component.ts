2910d7c87908c66f7f90584ca38dec13719a61d6
boboch86 <40466697+boboch86@users.noreply.github.com>
2020-09-08 15:14:58 +0200

    Exercise 12 - Custom validators in forms

--- ponyracer/src/app/register/register.component.ts
+++ ponyracer/src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pr-register',
@@ -9,23 +11,44 @@ import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
 export class RegisterComponent implements OnInit {
   userForm: FormGroup;
   loginCtrl: FormControl;
-  passwordCtrl: FormControl;
   birthYearCtrl: FormControl;
 
-  constructor(fb: FormBuilder) {
-    this.loginCtrl = fb.control('', [Validators.required]);
+  passwordForm: FormGroup;
+  passwordCtrl: FormControl;
+  confirmPasswordCtrl: FormControl;
+  registrationFailed: boolean;
+
+  static passwordMatch(group: FormGroup) {
+        const password = group.get('password').value;
+        const confirmPassword = group.get('confirmPassword').value;
+
+        return password !== confirmPassword ? { matchingError: true } : null;
+    }
+
+  constructor(fb: FormBuilder, private userService: UserService, private router: Router) {
+    this.loginCtrl = fb.control('', [Validators.required, Validators.minLength(3)]);
     this.passwordCtrl = fb.control('', [Validators.required]);
-    this.birthYearCtrl = fb.control('', [Validators.required]);
+    this.birthYearCtrl = fb.control('', [Validators.required, Validators.min(1900), Validators.max(new Date().getFullYear())]);
+    this.confirmPasswordCtrl = fb.control('', [Validators.required]);
+    this.passwordForm = fb.group(
+      { password: this.passwordCtrl, confirmPassword: this.confirmPasswordCtrl },
+      {validator: RegisterComponent.passwordMatch}
+      );
     this.userForm = fb.group({
-    login: this.loginCtrl,
-    password: this.passwordCtrl,
-    birthYear: this.birthYearCtrl
-    });
+      login: this.loginCtrl,
+      passwordForm: this.passwordForm,
+      birthYear: this.birthYearCtrl
+      });
    }
 
   ngOnInit() {
   }
 
     register() {
+      this.userService.register(this.userForm.value.login, this.userForm.value.passwordForm.password, this.userForm.value.birthYear)
+      .subscribe(
+        () => this.router.navigate(['/']),
+        () => this.registrationFailed = true
+      );
     }
-}
+  }
